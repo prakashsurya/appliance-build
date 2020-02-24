91140739faa662880addb54abd4900e2f1a40a0a
Prakash Surya <prakash.surya@delphix.com>
2020-02-24 12:38:30 -0800

    Move comment closer to code it's commenting on

--- scripts/build-ancillary-repository.sh
+++ scripts/build-ancillary-repository.sh
@@ -112,6 +112,29 @@ function build_ancillary_repository() {
 }
 
 #
+# Set UPSTREAM_BRANCH. This will determine which version of the linux package
+# mirror is used.
+#
+if [[ -z "$UPSTREAM_PRODUCT_BRANCH" ]]; then
+	echo "UPSTREAM_PRODUCT_BRANCH is not set."
+	if ! source "$TOP/branch.config" 2>/dev/null; then
+		echo "No branch.config file found in repo root."
+		exit 1
+	fi
+
+	if [[ -z "$UPSTREAM_BRANCH" ]]; then
+		echo "UPSTREAM_BRANCH parameter was not sourced from branch.config." \
+			"Ensure branch.config is properly formatted with e.g." \
+			"UPSTREAM_BRANCH=\"<upstream-product-branch>\""
+		exit 1
+	fi
+	echo "Defaulting to branch $UPSTREAM_BRANCH set in branch.config."
+else
+	UPSTREAM_BRANCH="$UPSTREAM_PRODUCT_BRANCH"
+fi
+echo "Running with UPSTREAM_BRANCH set to ${UPSTREAM_BRANCH}"
+
+#
 # The first-party packages produced by Delphix are stored in Amazon S3.
 # Thus, in order to populate the ancillary repository with these
 # packages, they must be downloaded from S3, so they can be then
@@ -141,29 +164,6 @@ function build_ancillary_repository() {
 #    and then uses that URI. This way, a naive user can not set any
 #    environment variables, and the script will work as expected.
 #
-
-#
-# Set UPSTREAM_BRANCH. This will determine which version of the linux package
-# mirror is used.
-#
-if [[ -z "$UPSTREAM_PRODUCT_BRANCH" ]]; then
-	echo "UPSTREAM_PRODUCT_BRANCH is not set."
-	if ! source "$TOP/branch.config" 2>/dev/null; then
-		echo "No branch.config file found in repo root."
-		exit 1
-	fi
-
-	if [[ -z "$UPSTREAM_BRANCH" ]]; then
-		echo "UPSTREAM_BRANCH parameter was not sourced from branch.config." \
-			"Ensure branch.config is properly formatted with e.g." \
-			"UPSTREAM_BRANCH=\"<upstream-product-branch>\""
-		exit 1
-	fi
-	echo "Defaulting to branch $UPSTREAM_BRANCH set in branch.config."
-else
-	UPSTREAM_BRANCH="$UPSTREAM_PRODUCT_BRANCH"
-fi
-echo "Running with UPSTREAM_BRANCH set to ${UPSTREAM_BRANCH}"
 
 AWS_S3_URI_VIRTUALIZATION=$(resolve_s3_uri \
 	"$AWS_S3_URI_VIRTUALIZATION" \
